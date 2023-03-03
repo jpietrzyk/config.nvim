b83fac60c8229280e7c91a4267726a34d9b2a1b8
Jacek Pietrzyk <jacek@grover.com>
2023-03-03 01:47:29 +0100

    Add git plugins

---
+++ lua/jck/plugins/diffview.lua
@@ -0,0 +1,9 @@
+-- diffview - nice git diff plugin
+-- https://github.com/sindrets/diffview.nvim_lsp
+
+return {
+  "sindrets/diffview.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
