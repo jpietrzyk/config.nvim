2a9779ce150bb430d373bfb18c043c3beb7ea482
Jacek Pietrzyk <jacek@grover.com>
2023-03-06 01:02:27 +0100

    NeoTree config

--- lua/jck/plugins/neo-tree.lua
+++ lua/jck/plugins/neo-tree.lua
@@ -12,4 +12,21 @@ return {
   keys = {
     { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
   },
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          ".DS_Store",
+          "thumbs.db",
+          "node_modules",
+        },
+      },
+    },
+  },
 }
+
